4a2c258cc991572ef5e5a24d642efac8d7aade28
Sandy_undefined <08sandysk@gmail.com>
2021-05-22 22:07:40 +0530

    project setup

---
+++ phase_one.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
+url = ""
+search = ""
+
+def amazon():
+
+    print("hello")
+
+amazon()
